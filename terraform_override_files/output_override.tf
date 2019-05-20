396ac8e30236b8358056f8df08a9a22a7a044981
warroyo <warroyo7199008@gmail.com>
2019-05-20 15:48:20 -0600

    update outputs

--- terraform_override_files/output_override.tf
+++ terraform_override_files/output_override.tf
@@ -3,9 +3,9 @@ output "subnet_mgmt_reserved" {
 }
 
 output "subnet_pks_reserved" {
-  value     = "${cidrhost(module.pks.pks_subnet_ip_cidr_range, 1)}-${cidrhost(module.pks.pks_subnet_ip_cidr_range, 9)}"
+  value     = "${cidrhost(module.pks.pks_subnet_cidrs, 1)}-${cidrhost(module.pks.pks_subnet_cidrs, 9)}"
 }
 
 output "subnet_pks_svc_reserved" {
-  value     = "${cidrhost(module.pks.services_subnet_ip_cidr_range, 1)}-${cidrhost(module.pks.services_subnet_ip_cidr_range, 9)}"
+  value     = "${cidrhost(module.pks.pks_services_subnet_cidrs, 1)}-${cidrhost(module.pks.pks_services_subnet_cidrs, 9)}"
 }
