b9e3b2f0bd3c7312148a889a4cf502955ace05be
warroyo <warroyo7199008@gmail.com>
2019-05-22 13:43:58 -0600

    fill in vars

---
+++ terraforming-k8s/variables.tf
@@ -0,0 +1,13 @@
+variable "instances" {}
+variable "env_name" {}
+
+variable "service_account_key" {}
+
+
+variable "project" {}
+
+variable "region" {}
+variable "cluster_name" {}
+variable "network" {}
+variable "dns_zone_name" {}
+variable "dns_zone_dns_name" {}
